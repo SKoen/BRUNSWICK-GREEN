29514365ad1bd82ebf7f105504211ee292853f72
rvm <rvmladenov@gmail.com>
2014-12-06 17:52:43 +0200

    added user login and partly user registration(due to different issues)

--- js/app.js
+++ js/app.js
@@ -1,12 +1,52 @@
 $(function(){
 	'use strict';
 
-	
+	function successCallback(data) {
+		$("#container").html(TEMPLATES.MAIN);
+	};
 
-	
-	
+	function errorCallback(data, err) {
+		var n = noty({
+			text        : 'The following error occured:<br />- ' + err.message,
+            type        : 'error',
+            dismissQueue: true,
+            closeWith   : ['click', 'backdrop'],
+            modal       : true,
+            layout      : 'center',
+            theme       : 'defaultTheme'
+        });
+	};
 
+	$(document).on('click', '#register', function(e){
+		// TODO: Implement this
+		e.preventDefault();
 
-	
+		var username = $("#username").val();
+		var password = $("#password").val();
+		var password2 = $("#password2").val();
+		var email = $("#email").val();
 
+		var user = new userPersistor(successCallback, errorCallback);
+		user.register(username, password, password2, email);
+	});
+
+	$(document).on('click', '#login', function(e) {
+		e.preventDefault();
+
+		var username = $("#username").val();
+		var password = $("#password").val();
+
+		var user = new userPersistor(successCallback, errorCallback);
+		user.login(username, password);
+	});
+
+	$("#section-forms").on('click', '#register-switch', function(e) {
+		e.preventDefault();
+		$("#section-forms").html(TEMPLATES.REGISTER);
+	});
+
+	$("#section-forms").on('click', '#login-switch', function(e) {
+		e.preventDefault();
+		$("#section-forms").html(TEMPLATES.LOGIN);
+	});
 });
--- js/userPersistor.js
+++ js/userPersistor.js
@@ -1,8 +1,5 @@
-var userPersistor = (function(){
+var userPersistor = (function() {
     'use strict';
-
-    var successCallback,
-    	errorCallback;
 
 	var appKey = 'jz77c8IPJpyGwYB2G3owJKVVlhgDiwhksSWkaXOx',
 		JSAPIKey = '5W6fpNbwpXn0opJWg2GbEkbD2Azo0J2ISRTIyJ2v';
@@ -12,30 +9,108 @@ var userPersistor = (function(){
     		throw new Error('Please provide the callback functions that will handle the results');
     	}
 
-    	successCallback = successCallback;
-    	errorCallback = errorCallback;
+    	this._successCallback = successCallback;
+    	this._errorCallback = errorCallback;
 
     	Parse.initialize(appKey, JSAPIKey);
     }
 
 	_self.prototype.login = function(user, pass) {
-		Parse.User.logIn(user, pass, { successCallback, errorCallback });
-	}
+		// TODO: Implement validation
+		// validateInput();
+		
+		var successCallback = this._successCallback;
+    	var errorCallback = this._errorCallback;
 
-	_self.prototype.register = function(user, pass, email) {
-		var user = new Parse.User();
-		user.set("username", user);
-		user.set("password", pass);
-		user.set("email", email);
+		Parse.User.logIn(user, pass, { 
+				success: function(user) { successCallback(user); },
+			 	error: function(user, err) { errorCallback(user, err); } 
+			 })
+	};
 
-		user.signUp(null, { successCallback, errorCallback });
+	_self.prototype.register = function(user, pass, pass2, email) {
+		// TODO: Implement validation
+		// validateInput();
+		 
+		if(pass != pass2) {
+			// return Parse.Promise.error(new Parse.Error(Parse.Error.OTHER_CAUSE, "The 2 passwords differ."));
+		}
+		
+		var successCallback = this._successCallback;
+    	var errorCallback = this._errorCallback;
+
+
+    	var newUser = new Parse.User();
+		newUser.set("username", user);
+		newUser.set("password", pass);
+		newUser.set("email", email);
+
+		newUser.signUp(null, {
+		  success: function(currUser) {
+
+		  		var query = new Parse.Query(Parse.Role);
+				query.equalTo("name", "registeredUser");
+				var r = query.find({
+				    success: function(Role) {
+				    	var role = Role[0];
+				    	var currUser = Parse.User.current();
+				  		
+				    	role.getUsers().add(currUser);
+				    	role.save();
+
+				  //   	currUser.setACL(role.getACL());
+
+				  //   	currUser.save().then(function() {
+						// 	alert('save to role was success');
+						// }, function(error) {
+						// 	alert('save role ERROR:' + error.message());
+						// });
+				    },
+				    error: function(newUser, err) { 
+				    	alert(err.message);
+				     }
+				});
+		  },
+		  error: function(user, error) {
+		    // Show the error message somewhere and let the user try again.
+		    alert("Error: " + error.code + " " + error.message);
+		  }
+		});
+	};
+
+	_self.prototype.addToDefaultRole = function() {
+		var query = new Parse.Query(Parse.Role);
+
+		query.equalTo("name", "registeredUser");
+
+		var r = query.find({
+		    success: function(Role) {
+		    	alert(Role);
+
+		    	// Role.getUsers().add(user);
+		    	// role.saveInBackground().then(function(){
+		    	// 	successCallback(user);
+		    	// });
+		    },
+		    error: function(user, err) { 
+		    	alert(err.message);
+		     }
+		});
 	}
 
 	_self.prototype.logout = function() {
 		// TODO: Implement this
 
 		Parse.User.logOut();
-	}
+	};
+
+	_self.prototype.getUser = function() {
+		return Parse.User.current();
+	};
+
+	_self.prototype.isLoggedIn = function() {
+
+	};
 
 	return _self;
 })();
