02ba26f74ccb5ab403909073ead3c378174fb048
m4r71n85 <martin.tsekov.bg@gmail.com>
2014-12-07 23:36:13 +0200

    update on previous commit

--- js/app/controllers/myAlbumsController.js
+++ js/app/controllers/myAlbumsController.js
@@ -3,6 +3,7 @@
     var myAlbums = new albumPersistor();
     $(document).on('click', '#my-albums-controller  #btn-add-album', function (e) {
         var albumTitle = $("#my-albums-controller #txt-album-title").val();
+        $("#my-albums-controller #txt-album-title").val('');
         var currentUser = user.getUser();
 
         myAlbums.createAlbum(albumTitle, currentUser, successCreate);
